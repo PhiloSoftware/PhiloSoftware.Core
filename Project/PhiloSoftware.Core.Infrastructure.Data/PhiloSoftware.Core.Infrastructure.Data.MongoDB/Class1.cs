672b621cb2502c1a13951d472fc2947d459885d2
Morgan Philo <morgan.philo@teamdatalogic.com>
2015-01-18 16:26:12 +0000

    Better structure

---
+++ Project/PhiloSoftware.Core.Infrastructure.Data/PhiloSoftware.Core.Infrastructure.Data.MongoDB/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhiloSoftware.Core.Infrastructure.Data.MongoDB
+{
+    public class Class1
+    {
+    }
+}
