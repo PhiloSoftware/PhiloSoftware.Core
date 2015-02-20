dcb7027a90d4964bada5ec7af7a235bd7c78b437
Morgan Philo <morgan.philo@teamdatalogic.com>
2015-02-20 20:32:30 +0000

    minor fixes

--- Project/PhiloSoftware.Core.Infrastructure/Data/Factory/EntityFactory.cs
+++ Project/PhiloSoftware.Core.Infrastructure/Data/Factory/EntityFactory.cs
@@ -1,17 +1,16 @@
-﻿using PhiloSoftware.Core.Infrastructure.Data.Repository;
-using PhiloSoftware.Core.Infrastructure.Implementation;
+﻿using PhiloSoftware.Core.Infrastructure.Implementation;
 
 namespace PhiloSoftware.Core.Infrastructure.Data.Factory
 {
     public abstract class EntityFactory<T> where T : Entity
     {
-        protected EntityFactory(Repository<T> repository, IGenerateSequentialGuids guidGenerator)
+        protected EntityFactory(IRepository<T> repository, IGenerateSequentialGuids guidGenerator)
         {
             Repository = repository;
             GuidGenerator = guidGenerator;
         }
 
-        public Repository<T> Repository { get; private set; }
+        public IRepository<T> Repository { get; private set; }
         protected IGenerateSequentialGuids GuidGenerator { get; set; }
     }
 }
