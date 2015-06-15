dbe5075b251d1330060b3f22dd2b133ed7e37e4c
MorganPhilo <morganphilo@hotmail.com>
2015-06-15 20:44:38 +0100

    tidy up

--- Project/PhiloSoftware.Core.Data.MongoDB/MongoDBConventions.cs
+++ Project/PhiloSoftware.Core.Data.MongoDB/MongoDBConventions.cs
@@ -8,28 +8,12 @@ namespace PhiloSoftware.Core.Infrastructure.Data.MongoDB
 {
     public static class MongoDBConventions
     {
-        public static void IEntityIdMappingConvention<T>()
+        public static void IEntityIdMappingConvention()
         {
             var noIdConventions = new ConventionProfile();
             noIdConventions.SetIdMemberConvention(new NamedIdMemberConvention("Id")); // no names
-            ConventionRegistry.Register("noIdConvention", noIdConventions, t => t == typeof(T));
-        }
-
-        public static void IEntityIdMappingConvention(params Type[] types)
-        {
-            var noIdConventions = new ConventionProfile();
-            noIdConventions.SetIdMemberConvention(new NamedIdMemberConvention("ID")); // no names
-
-            types.ToList().ForEach(type => ConventionRegistry.Register("noIdConvention", noIdConventions, t => t == type));
-        }
-
-        public static void DoNotMapProperty<T,P>(Expression<Func<T, P>> propertyLambda)
-        {
-            BsonClassMap.RegisterClassMap<T>(cm =>
-            {
-                cm.AutoMap();
-                cm.UnmapProperty(propertyLambda);
-            });
+            noIdConventions.SetIgnoreExtraElementsConvention(new AlwaysIgnoreExtraElementsConvention());
+            ConventionRegistry.Register("noIdConvention", noIdConventions, t => true);
         }
     }
 }
